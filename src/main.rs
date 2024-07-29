5255fb78e81f961f1bd2ddf68f482e75eeb92bda
Harkabus <j.martenek@azet.sk>
2024-07-29 22:56:20 +0200

    learning ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,42 @@
-use std::{
-    io::{self, stdin, stdout},
-    vec,
-};
+use std::io;
 
-use crossterm::terminal;
-use ratatui::{
-    buffer::Buffer,
-    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    layout::{Alignment, Rect},
-    prelude::CrosstermBackend,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Span, Text},
-    widgets::{
-        block::{Position, Title},
-        Block, Paragraph, Widget,
-    },
-    Frame, Terminal,
-};
+use app::App;
+use crossterm::event::{self, Event};
+use ratatui::Frame;
 
+mod app;
 mod tui;
-
-#[derive(Debug, Default, Clone, Copy)]
-struct App {
-    counter: u8,
-    exit: bool,
-}
-
-impl App {
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-
-    fn decrement_counter(&mut self) {
-        self.counter -= 1;
-    }
-
-    fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| {
-                self.redner_frame(frame);
-            })?;
-            self.handle_events();
-        }
-        Ok(())
-    }
-
-    fn redner_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event);
-            }
-            _ => (),
-        }
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
-            _ => (),
-        }
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from("Blazingly fast Counter App".bold());
-        let description = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<q> ".blue().bold(),
-        ]));
-
-        let block = Block::bordered()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                description
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            );
-
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Count: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
-
-        let p = Paragraph::new(counter_text).centered().block(block);
-
-        p.render(area, buf);
-    }
-}
 
 fn main() -> io::Result<()> {
     let mut terminal = tui::init()?;
-    let app_result = App::default().run(&mut terminal);
+    let app = App::new();
+    let res = run_app(&mut terminal, &app);
+
+    match res {
+        Ok(val) => {
+            if val {
+                app.print_json()?;
+            }
+        }
+        Err(err) => eprint!("{err:?}"),
+    }
     tui::restore()?;
-    app_result
+
+    Ok(())
 }
+
+fn run_app(terminal: &mut tui::Tui, app: &App) -> io::Result<bool> {
+    loop {
+        terminal.draw(|f| ui(f, app))?;
+        handle_events(event::read()?);
+    }
+}
+
+fn handle_events(event: Event) {
+    match event {
+        Event::Key(key_event) => println!("{:?}", key_event.code),
+        _ => {}
+    }
+}
+
+fn ui(f: &mut Frame, app: &App) {}
--- src/tui.rs
+++ src/tui.rs
@@ -1,6 +1,7 @@
 use std::io::{self, stdout};
 
 use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
     execute, queue,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     QueueableCommand,
@@ -11,14 +12,14 @@ pub type Tui = Terminal<CrosstermBackend<io::Stdout>>;
 
 pub fn init() -> io::Result<Tui> {
     let mut stdout = stdout();
-    execute!(stdout, EnterAlternateScreen)?;
+    execute!(stdout, EnterAlternateScreen, DisableMouseCapture)?;
     enable_raw_mode()?;
     let terminal = Terminal::new(CrosstermBackend::new(stdout))?;
     Ok(terminal)
 }
 
 pub fn restore() -> io::Result<()> {
-    execute!(stdout(), LeaveAlternateScreen)?;
+    execute!(stdout(), LeaveAlternateScreen, EnableMouseCapture)?;
     disable_raw_mode()?;
     Ok(())
 }
