eeaa03c8b4c6685b9874a6228c70a2103d1c048d
Harkabus <j.martenek@azet.sk>
2024-09-21 21:33:37 +0200

    refactoring app, added draw method

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,13 @@
-use std::io;
+use std::{error::Error, io};
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use ratatui::{
+    layout::{Direction, Layout},
+    Frame,
+};
 
 use crate::{
+    components::KeyState,
     database::{Db, InputState},
     tui, ui,
 };
@@ -12,10 +17,10 @@ pub enum Mode {
     Insert,
 }
 
-pub enum AppState {
-    Receiving(ViewState),
-    Editing,
-}
+// pub enum AppState {
+//     Receiving(ViewState),
+//     Editing,
+// }
 
 pub enum ViewState {
     Main,
@@ -28,26 +33,26 @@ pub enum ViewState {
 
 pub struct App {
     pub current_view: Option<ViewState>,
-    pub app_state: Option<AppState>,
-    pub mode: Mode,
+    //    pub app_state: Option<AppState>,
+    // pub mode: Mode,
     pub db: Db,
-    pub display_dialog: bool,
-    pub display_append: bool,
-    pub error_message: Option<io::Error>, // go-to dialog
-    pub input: String,
+    //  pub display_dialog: bool,
+    //    pub display_append: bool,
+    //    pub error_message: Option<io::Error>, // go-to dialog
+    //    pub input: String,
 }
 
 impl App {
     pub fn new() -> Self {
         Self {
             current_view: Some(ViewState::Main),
-            app_state: None,
-            mode: Mode::Normal,
+            // app_state: None,
+            // mode: Mode::Normal,
             db: Db::new().expect("Could not create DB instance"),
-            display_dialog: false,
-            display_append: false,
-            error_message: None,
-            input: String::new(),
+            // display_dialog: false,
+            // display_append: false,
+            // error_message: None,
+            // input: String::new(),
         }
     }
 
@@ -56,7 +61,11 @@ impl App {
             if let Ok(event) = event::read() {
                 self.handle_event(event);
             }
-            terminal.draw(|frame| ui::draw_ui(self, frame))?;
+            terminal.draw(|frame| {
+                if let Ok(_) = self.draw(frame) {
+                    todo!()
+                }
+            });
 
             if let Some(ViewState::Exiting) = self.current_view {
                 break;
@@ -68,229 +77,40 @@ impl App {
     fn handle_event(&mut self, event: Event) {
         match event {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
+                self.handle_key_event(key_event);
             }
             _ => {}
         }
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        if self.display_dialog {
-            match key_event.code {
-                KeyCode::Char('c') => self.change_view(ViewState::Create),
-                KeyCode::Char('r') => self.change_view(ViewState::Read),
-                KeyCode::Char('d') => self.change_view(ViewState::Delete),
-                KeyCode::Char('q') => self.change_view(ViewState::Exiting),
-
-                _ => {}
-            }
-            self.display_dialog = false;
-            return;
-        };
-
-        match key_event {
-            KeyEvent {
-                modifiers: KeyModifiers::CONTROL,
-                code: KeyCode::Char('o'), // open
-                ..
-            } => {
-                self.display_dialog = true;
-            }
-            KeyEvent {
-                code: KeyCode::Esc, ..
-            } => {
-                self.error_message = None;
-            }
-            _ => {}
-        }
-
-        match self.current_view {
-            Some(ViewState::Main) => match key_event.code {
-                KeyCode::Char('q') => self.exit(),
-                _ => {}
-            },
-            Some(ViewState::Create) => match key_event.code {
-                KeyCode::Char(ch) => self.input.push(ch),
-                KeyCode::Backspace => {
-                    self.input.pop();
-                }
-                KeyCode::Enter => {
-                    if !self.input.is_empty() {
-                        self.db.db_name = Some(self.input.clone());
-                    }
-                    if let Err(err) = self.db.create_db() {
-                        self.error_message = Some(err);
-                    }
-                    self.change_app_state();
-                    // self.db.select_table(&self.current_view).unwrap();
-                    self.current_view = None;
-                    self.input.clear();
-                    return;
-                }
-
-                _ => {}
-            },
-            Some(ViewState::Read) => match key_event.code {
-                KeyCode::Char(ch) => self.input.push(ch),
-                KeyCode::Backspace => {
-                    self.input.pop();
-                }
-                KeyCode::Enter => {
-                    if !self.input.is_empty() {
-                        self.db.db_name = Some(self.input.clone());
-                    }
-                    if let Err(err) = self.db.open_db_if_exists() {
-                        self.error_message = Some(err);
-                    }
-
-                    self.change_app_state();
-                    // self.db.select_table(&self.current_view).unwrap();
-
-                    self.current_view = None;
-                    self.input.clear();
-                }
-                _ => {}
-            },
-            Some(ViewState::Update) => {
-                if self.display_append {
-                    // modify table component
-                    match key_event.code {
-                        KeyCode::Char('c') => {
-                            self.db.add_column(self.db.col_name.clone());
-                            self.db.col_name.clear()
-                        }
-                        KeyCode::Char(ch) => {
-                            self.db.column.push(ch);
-                        }
-                        KeyCode::Backspace => {
-                            self.db.col_name.pop();
-                        }
-                        _ => {}
-                    }
-                }
-                match key_event.code {
-                    KeyCode::Esc => {
-                        if matches!(self.mode, Mode::Insert) {
-                            self.mode = Mode::Normal;
-                        }
-                    }
-                    KeyCode::Char('i') => {
-                        if matches!(self.mode, Mode::Normal) {
-                            self.mode = Mode::Insert;
-                            return;
-                        }
-                    }
-
-                    KeyCode::Char('a') => {
-                        if matches!(self.mode, Mode::Normal) {
-                            self.display_append = true;
-                        }
-                    }
-
-                    _ => {}
-                }
-                match key_event.code {
-                    KeyCode::Up => self.db.cursor.prev_row(),
-                    KeyCode::Down => self.db.cursor.next_row(),
-                    KeyCode::Left => self.db.cursor.prev_col(),
-                    KeyCode::Right => self.db.cursor.next_col(),
-                    _ => {}
-                }
-                if matches!(self.mode, Mode::Normal) {
-                    match key_event {
-                        KeyEvent {
-                            code: KeyCode::Char('n'),
-                            modifiers: KeyModifiers::NONE,
-                            ..
-                        } => self.db.add_record(),
-                        KeyEvent {
-                            code: KeyCode::Char('n'),
-                            modifiers: KeyModifiers::CONTROL,
-                            ..
-                        } => self.db.pop_record(),
-
-                        _ => {}
-                    }
-                }
-                if matches!(self.mode, Mode::Insert) {
-                    match key_event.code {
-                        KeyCode::Char('b') => self.db.add_column("test integer".to_owned()),
-                        KeyCode::Char(ch) => self.db.cursor.update_item(ch),
-                        KeyCode::Backspace => self.db.cursor.pop_item(),
-                        _ => {}
-                    }
-                }
-            }
-            Some(ViewState::Delete) => match key_event.code {
-                _ => {}
-            },
-            _ => {}
-        }
-
-        match self.app_state {
-            Some(AppState::Receiving(ViewState::Create)) => {
-                match key_event.code {
-                    KeyCode::Enter => {
-                        self.db.parse_table().expect("Ano, Hej, mhmm");
-                        self.app_state = None;
-                        self.change_view(ViewState::Update);
-                    }
-                    _ => {}
-                }
-                match self.db.input_state {
-                    InputState::Table => match key_event.code {
-                        KeyCode::Char(ch) => {
-                            self.db.table_name.push(ch);
-                        }
-                        KeyCode::Backspace => {
-                            self.db.table_name.pop();
-                        }
-                        KeyCode::Tab => {
-                            self.db.toggle_input_state();
-                        }
-                        _ => {}
-                    },
-                    InputState::Column => match key_event.code {
-                        KeyCode::Char(ch) => {
-                            self.db.column.push(ch);
-                        }
-                        KeyCode::Backspace => {
-                            self.db.column.pop();
-                        }
-                        KeyCode::Tab => {
-                            self.db.toggle_input_state();
-                        }
-                        _ => {}
-                    },
-                }
-                // self.db.records.push(10.into());
-            }
-            Some(AppState::Receiving(ViewState::Read)) => {}
-            Some(AppState::Editing) => {}
-            _ => {}
-        }
-    }
-
-    // for item
-    fn change_view(&mut self, view: ViewState) {
-        self.current_view = Some(view);
-    }
-
-    fn change_app_state(&mut self) -> io::Result<()> {
-        if self.current_view.is_none() {
-            return Err(io::Error::new(
-                io::ErrorKind::Other,
-                "state has to be initialized at this point",
-            ));
-        }
-
-        match self.current_view.as_ref().unwrap() {
-            ViewState::Create => self.app_state = Some(AppState::Receiving(ViewState::Create)),
-            ViewState::Read => self.app_state = Some(AppState::Receiving(ViewState::Read)),
-            _ => {}
-        }
+    fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<(), Box<dyn std::error::Error>> {
+        let chunks = Layout::default().direction(Direction::Horizontal);
         Ok(())
     }
+
+    fn draw(&mut self, f: &mut Frame) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
+    // for item
+    //  fn change_view(&mut self, view: ViewState) {
+    //      self.current_view = Some(view);
+    //  }
+
+    //  fn change_app_state(&mut self) -> io::Result<()> {
+    //      if self.current_view.is_none() {
+    //          return Err(io::Error::new(
+    //              io::ErrorKind::Other,
+    //              "state has to be initialized at this point",
+    //          ));
+    //      }
+
+    //      match self.current_view.as_ref().unwrap() {
+    //          ViewState::Create => self.app_state = Some(AppState::Receiving(ViewState::Create)),
+    //          ViewState::Read => self.app_state = Some(AppState::Receiving(ViewState::Read)),
+    //          _ => {}
+    //      }
+    //      Ok(())
+    //  }
 
     fn exit(&mut self) {
         //
